23c11c505c0e0a3ad7654e0c6b76a3a85be44510
jambis <imjaamees@gmail.com>
2020-02-14 10:39:03 -0600

    Completed oop and list comprehensions

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,31 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+#Vehicle Class
+class Vehicle():
+    pass
+
+#FlgithVehicle Class
+class FlightVehicle(Vehicle):
+    pass
+
+#Starship Class
+class Starship(FlightVehicle):
+    pass
+
+#GroundVehicle Class
+class GroundVehicle(Vehicle):
+    pass
+
+#Airplane Class
+class Airplane(FlightVehicle):
+    pass
+
+#Car Class
+class Car(GroundVehicle):
+    pass
+
+#Motorcycle Class
+class Motorcycle(GroundVehicle):
+    pass
